f5e9d6e6a7143e41d4a57521d01fc311eac6b900
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2023-11-18 03:30:21 +0900

    exception handler controller upload

--- src/main/java/com/example/idealselect/controller/api/AuthApiController.java
+++ src/main/java/com/example/idealselect/controller/api/AuthApiController.java
@@ -4,6 +4,8 @@ import com.example.idealselect.service.UserServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
@@ -14,17 +16,20 @@ public class AuthApiController {
     private final UserServiceImpl userService;
 
     @PostMapping("/login")
-    public void login(@RequestParam("userId") String userId, @RequestParam("password") String password, HttpServletRequest request, HttpServletResponse response){
+    public ResponseEntity login(@RequestParam("userId") String userId, @RequestParam("password") String password, HttpServletRequest request, HttpServletResponse response){
         userService.login(userId, password, request, response);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("/join")
-    public void join(@RequestParam String userId, @RequestParam String userName, @RequestParam String password){
+    public ResponseEntity join(@RequestParam String userId, @RequestParam String userName, @RequestParam String password){
         userService.join(userId, userName, password);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping("/logOut")
-    public void logOut(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity logOut(HttpServletRequest request, HttpServletResponse response) {
         userService.logOut(request, response);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
